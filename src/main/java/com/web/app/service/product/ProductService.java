81b027590e6b747d03e47fa467bd72168f57d0e3
lrul <lrul@cn.ibm.com>
2017-08-05 20:44:02 +0800

    Ignore dev file

---
+++ src/main/java/com/web/app/service/product/ProductService.java
@@ -0,0 +1,15 @@
+package com.web.app.service.product;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * User: Administrator
+ * Date: 2017-08-05
+ * Time: 14:08
+ */
+@Service
+@Transactional
+public class ProductService {
+
+}

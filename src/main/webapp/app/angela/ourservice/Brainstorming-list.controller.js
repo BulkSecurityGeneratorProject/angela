d0891c0a5e9c5e7945fe20d3675b658741c5d39c
zjlipl-cn <zjlipl@cn.ibm.com>
2017-09-22 18:28:18 +0800

    9.22

--- src/main/webapp/app/angela/ourservice/Brainstorming-list.controller.js
+++ src/main/webapp/app/angela/ourservice/Brainstorming-list.controller.js
@@ -8,6 +8,23 @@
     BrainstormingListController.$inject = ['$scope', 'Principal', 'LoginService', '$state'];
 
     function BrainstormingListController ($scope, Principal, LoginService, $state) {
+        var vm = this;
 
+        vm.windowHeight = window.innerHeight;
+		vm.headerHeight = 80;
+		vm.footerHeight = 40;
+/*    	vm.directive('autoHeight', function($window){
+		  return {
+		    restrict: 'A',
+		    scope: {},
+		    function($scope, element, attrs){
+		      var winowHeight = $window.innerHeight;
+		      var headerHeight = 80;
+		      var footerHeight = 40;
+		      console.log(element);
+		      element.css('min-height', (winowHeight - headerHeight - footerHeight) + 'px');  
+		    }
+		  };
+		});*/
     }
 })();
